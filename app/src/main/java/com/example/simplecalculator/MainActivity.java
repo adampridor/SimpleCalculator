08d85bcf0df2b85ae2b10f6a7308f3ce77a29d8a
adampridor <adam@pridor.com>
2023-12-08 10:53:11 +0200

    add java

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void onBtnClicked(View view) {
+        // Get view handle using the view id
+        EditText et1 = findViewById(R.id.Num1);
+
+        // Get the view’s text
+        String et1Text = et1.getText().toString();
+
+        // Cast the String into Integer
+        Integer num1 = Integer.valueOf(et1Text);
+
+        // Get view handle using the view id
+        EditText et2 = findViewById(R.id.Num2);
+
+        // Get the view’s text
+        String et2Text = et2.getText().toString();
+
+        // Cast the String into Integer
+        Integer num2 = Integer.valueOf(et2Text);
+
+        Integer result = null;
+        if (view.getId() == R.id.btnPlus)
+            result = num1 + num2;
+        if (view.getId() == R.id.btnMinus)
+            result = num1 - num2;
+        if (view.getId() == R.id.btnMult)
+            result = num1 * num2;
+        if (view.getId() == R.id.btnDiv)
+            result = num1 / num2;
+
+        if (result != null) {
+            TextView tvRes = findViewById(R.id.tvResult);
+            tvRes.setText(result.toString());
+        }
     }
 }
